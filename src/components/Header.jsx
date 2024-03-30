036e266feb51edd0c5ffabc1c94b2ca93754e39d
fnpassong <fa_pa01@hotmail.com>
2024-03-29 21:52:03 -0500

    primer commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import '../scss/components/header.scss';
+import logo from '../assets/shared/logo.svg';
+import Boton from "./common/Boton.jsx";
+
+const Header =()=> {
+    return(
+        <header className='hdr' >
+            <div className="hdr__wrapper wrapper">
+                <img src= { logo } alt="logo-empresa"/>
+                <Boton/>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/common/Boton.jsx
@@ -0,0 +1,9 @@
+import '../../scss/components/boton.scss';
+
+const Boton =()=> {
+    return(
+        <h3 className='btn text-upper'>pre-order now</h3>
+    )
+}
+
+export default Boton;
