4cd91d7ba536f58604913027d3cb728afa9765ea
fnpassong <fa_pa01@hotmail.com>
2024-03-30 09:30:29 -0500

    se ingrsó estilos mechanical

--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -1,18 +1,18 @@
 import Boton from "./common/Boton.jsx";
-
+import '../scss/components/master.scss';
 const Master =()=> {
     return(
         <section className='master'>
             <div className="master__text">
-                <h1 className="master__main-title">
+                <h1 className="master__main-title text-upper text-blue">
                     typemaster keyboard
                 </h1>
-                <p className="master__main-paragraph">
+                <p className="master__main-paragraph text-grey">
                     Improve your productivity and gaming without breaking the bank. Upgrade to a high quality mechanical typing experience.
                 </p>
                 <div className="master__btn-group">
                     <Boton/>
-                    <p>release on 5/27</p>
+                    <p className='text-upper text-grey'>release on 5/27</p>
                 </div>
             </div>
             <div className="master__img-group1">
@@ -25,10 +25,10 @@ const Master =()=> {
                 <div className="master__glass"></div>
             </div>
             <div className="master__mechanical">
-                <h2 className="master__title">
+                <h2 className="master__title text-upper text-center text-blue">
                     mechanical wireless keyboard
                 </h2>
-                <p className="master__paragraph">
+                <p className="master__paragraph text-grey">
                     The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety of switches and keycaps, along with reliable wireless connectivity.
                 </p>
             </div>
